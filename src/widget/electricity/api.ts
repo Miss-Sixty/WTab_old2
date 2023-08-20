ff8a16559ae008065b21a7d80536d3f942fb376f
xihe <im0k@outlook.com>
2023-08-21 00:48:09 +0800

    优化

--- src/widget/electricity/api.ts
+++ src/widget/electricity/api.ts
@@ -1,12 +1,14 @@
 import request from '@/utils/request'
 import dayjs from 'dayjs'
-
+const VITE_ELECTRICITY_UUID = '6850685d6dda8eb95628e6a85b551216'
+const VITE_ELECTRICITY_TOKEN =
+  'ed130a4d01e5b6435e88ba4e182351b9841a02183e4e785412ec1c7e1d565f3d836f2f64954c92839424af28a3e462fbc9454804c14b825301e2fb86ab1f7360'
 // 电费详情
 export function elemeterInfoApi() {
   return request({
-    url: `${import.meta.env.VITE_API_URL}/admin/elemeter/${import.meta.env.VITE_ELECTRICITY_UUID}`,
+    url: `${import.meta.env.VITE_API_URL}/admin/elemeter/${VITE_ELECTRICITY_UUID}`,
     headers: {
-      'x-access-token': import.meta.env.VITE_ELECTRICITY_TOKEN
+      'x-access-token': VITE_ELECTRICITY_TOKEN
     }
   })
 }
@@ -14,11 +16,9 @@ export function elemeterInfoApi() {
 // 充值记录
 export function chargeRecordApi() {
   return request({
-    url: `${import.meta.env.VITE_API_URL}/admin/elemeter/charge_record/${
-      import.meta.env.VITE_ELECTRICITY_UUID
-    }`,
+    url: `${import.meta.env.VITE_API_URL}/admin/elemeter/charge_record/${VITE_ELECTRICITY_UUID}`,
     headers: {
-      'x-access-token': import.meta.env.VITE_ELECTRICITY_TOKEN
+      'x-access-token': VITE_ELECTRICITY_TOKEN
     },
     params: {
       start_time: '1675304460000',
@@ -30,11 +30,9 @@ export function chargeRecordApi() {
 // 用电记录
 export function amountRecordApi() {
   return request({
-    url: `${import.meta.env.VITE_API_URL}/admin/elemeter/amount_record/${
-      import.meta.env.VITE_ELECTRICITY_UUID
-    }`,
+    url: `${import.meta.env.VITE_API_URL}/admin/elemeter/amount_record/${VITE_ELECTRICITY_UUID}`,
     headers: {
-      'x-access-token': import.meta.env.VITE_ELECTRICITY_TOKEN
+      'x-access-token': VITE_ELECTRICITY_TOKEN
     },
     params: {
       start_time: dayjs().subtract(31, 'day').valueOf(),
