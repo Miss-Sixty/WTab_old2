c7882e901a2e8f2b47645ee72797288d12637a2b
xihe <im0k@outlook.com>
2023-08-21 00:29:07 +0800

    优化

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import viteCompression from 'vite-plugin-compression'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -21,6 +22,36 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()]
     }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'script',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      // devOptions: {
+      //   enabled: true
+      // },
+      includeAssets: ['favicon.ico', 'miaomiao.svg'],
+      manifest: {
+        name: 'WTab',
+        short_name: 'WTab',
+        description: 'WTab标签页',
+        theme_color: '#ffffff',
+        icons: [
+          // {
+          //   src: '/favicon.ico',
+          //   sizes: '64x64 32x32 24x24 16x16',
+          //   type: 'image/x-icon',
+          //   purpose: 'any'
+          // },
+          {
+            src: '/miaomiao.svg',
+            sizes: '512x512 192x192',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
   ],
   build: {
     minify: 'terser', // 混淆器，terser构建后文件体积更小
@@ -30,6 +61,22 @@ export default defineConfig({
         drop_debugger: true // 打包时删除debugger
       }
     }
+    // rollupOptions: {
+    //   output: {
+    //     // manualChunks 配置
+    //     manualChunks: {
+    //       // 两个文件合并成一个helloWorld文件
+    //       settings: [
+    //         'src/components/About.vue',
+    //         'src/components/Settings/index.vue',
+    //         'src/components/Wallpaper/index.vue',
+    //         'src/components/Icon.vue',
+    //         'src/components/Dialog.vue',
+    //         'src/components/Header.vue'
+    //       ]
+    //     }
+    //   }
+    // }
   },
   resolve: {
     alias: {
